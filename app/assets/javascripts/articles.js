90f14cbd6e018882ddda78bd196caceeb0e0a3f6
Memfis <badalloffsh@gmail.com>
2014-07-17 22:39:05 +0500

    Create article done + nicEdit

---
+++ app/assets/javascripts/articles.js
@@ -0,0 +1,32 @@
+
+// Visual text editor
+//= require 'vanila/nicEdit'
+
+// NicEdit
+bkLib.onDomLoaded(function() {
+    var myNicEditor = new nicEditor({
+        buttonList : [
+            'bold',
+            'italic',
+            'underline',
+            'forecolor',
+            'left',
+            'center',
+            'right',
+            'justify'
+        ]
+    });
+    myNicEditor.setPanel('edit_buttons');
+    myNicEditor.addInstance('styled_message');
+});
+setTimeout(function () {
+    var nic_width = $('.nicEdit-panel').width();
+    $('.nicEdit-container').css('width', nic_width);
+    $('.nicEdit-main').css('width', nic_width-24);
+}, 2000);
+$(window).resize(function() {
+    var nic_width = $('.nicEdit-panel').width();
+    $('.nicEdit-container').css('width', nic_width);
+    $('.nicEdit-main').css('width', nic_width-24);
+});
+// \NicEdit
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,6 @@
 Rails.application.routes.draw do
-  get 'articles/index'
 
-  get 'articles/show'
-
-  get 'articles/edit'
-
-  get 'articles/update'
-
-  get 'articles/new'
-
-  get 'articles/create'
-
-  get 'articles/destroy'
+  resources :articles
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
